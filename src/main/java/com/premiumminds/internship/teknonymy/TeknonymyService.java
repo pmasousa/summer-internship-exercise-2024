b23e0c45382533f25cc63893fdad907691541f5b
Pedro Sousa <sousaampedro@gmail.com>
2024-05-19 11:39:55 +0100

    feat(getTeknonymy): initial version

--- src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
+++ src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
@@ -11,6 +11,67 @@ class TeknonymyService implements ITeknonymyService {
    * @return String which is the Teknonymy Name 
    */
   public String getTeknonymy(Person person) {
-    throw new RuntimeException("Not Implemented Yet");
+    String TeknonymyName = "";
+    if (person.children() == null) {
+      return TeknonymyName;
+    }
+    
+    int childDepth = getLowestDepth(person);
+
+    Person oldestChild = getOldestChild(person, childDepth, 0);
+
+    if (childDepth > 2) {
+      for (int i = 0; i < childDepth - 2; i++) {
+        TeknonymyName += "great-";
+      }
+      TeknonymyName += "grand";
+    }
+    if (person.sex() == 'M') {
+      TeknonymyName += "father";
+    } else {
+      TeknonymyName += "mother";
+    }
+    TeknonymyName += " of " + oldestChild.name();
+
+    return TeknonymyName;
   };
+
+  private Integer getLowestDepth(Person person) {
+    int lowestDepth = 0;
+    if (person.children() == null) {
+      return lowestDepth;
+    }
+    for (Person child : person.children()) {
+      int childDepth = getLowestDepth(child);
+      if (childDepth > lowestDepth) {
+        lowestDepth = childDepth;
+      }
+    }
+    return lowestDepth + 1;
+  }
+
+  private Person getOldestChild(Person person, int depth, int currentDepth) {
+    Person oldestChild = null;
+    Person currentChild = null;
+    if (currentDepth == depth) {
+      return person;
+    }
+    if (person.children() == null) {
+      return null;
+    }
+    for (Person child : person.children()) {
+      currentChild = getOldestChild(child, depth, currentDepth + 1);
+      if (currentChild == null) {
+        continue;
+      }
+      if (oldestChild == null) {
+        oldestChild = currentChild;
+      } else {
+        if (currentChild.dateOfBirth().isBefore(oldestChild.dateOfBirth())) {
+          oldestChild = currentChild;
+        }
+      }
+    }
+    return oldestChild;
+  }
 }
